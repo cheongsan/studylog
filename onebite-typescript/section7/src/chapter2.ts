2b6c2e6f83d5a848011f06d18b823758ebb5a1a3
Cheongsan Kim <land@cheongsan.do>
2025-02-27 23:59:22 +0900

    [#️⃣ TypeScript 이정환 - 한 입 크기로 잘라먹는 타입스크립트] map, forEach 메서드 타입 정의하기

---
+++ onebite-typescript/section7/src/chapter2.ts
@@ -0,0 +1,43 @@
+/*
+ * map 메서드
+ */
+
+const arr = [1, 2, 3];
+const newArr = arr.map((it) => it * 2); // [2, 4, 6]
+
+// map 메서드 타입 정의
+function map<T, U>(arr: T[], callback: (item: T) => U) {
+  let result = [];
+  for (let i = 0; i < arr.length; i++) {
+    result.push(callback(arr[i]));
+  }
+
+  return result;
+}
+
+map(arr, (it) => it * 2);
+
+// T = string[]
+// parseInt -> U에 들어와서 number 타입 추론
+map(["hi", "hello"], (it) => parseInt(it));
+
+/*
+ * forEach
+ */
+
+const arr2 = [1, 2, 3];
+arr2.forEach((it) => console.log(it));
+
+function forEach<T>(arr: T[], callback: (item: T) => void) {
+  for (let i = 0; i < arr.length; i++) {
+    callback(arr[i]);
+  }
+}
+
+forEach(arr2, (it) => {
+  console.log(it.toFixed());
+});
+
+forEach(["123", "456"], (it) => {
+  it;
+});
