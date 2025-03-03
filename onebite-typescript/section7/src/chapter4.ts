8043d656986b6217936fd0294820a082944266ea
KIM, CHEONGSAN <88742184+cheongsan@users.noreply.github.com>
2025-03-04 04:30:10 +0900

    [#️⃣ TypeScript 이정환 - 한 입 크기로 잘라먹는 타입스크립트] 제네릭 클래스

---
+++ onebite-typescript/section7/src/chapter4.ts
@@ -0,0 +1,51 @@
+/**
+ * 제네릭 클래스
+ */
+
+
+// class NumberList {
+//     // 생성자
+//     constructor(private list: number[]){}
+
+//     // 메서드
+//     push(data: number){
+//         this.list.push(data)
+//     }
+
+//     pop(){
+//         return this.list.pop();
+//     }
+
+//     print(){
+//         console.log(this.list);
+//     }
+// }
+
+// number[] -> T도 number로 추론
+class List<T> {
+    // 생성자
+    constructor(private list: T[]){}
+
+    // 메서드
+    push(data: T){
+        this.list.push(data)
+    }
+
+    pop(){
+        return this.list.pop();
+    }
+
+    print(){
+        console.log(this.list);
+    }
+}
+
+const numberList = new List([1, 2, 3]);
+numberList.pop();
+numberList.push(4);
+numberList.print();
+
+
+// T = string으로 추론
+const stringList = new List(["1", "2"]);
+stringList.push("hello");
