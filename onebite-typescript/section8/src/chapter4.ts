cfa185914cc7305fb35ca878ae8e3794f663b849
Cheongsan Kim <land@cheongsan.do>
2025-03-05 22:21:33 +0900

    [#️⃣ TypeScript 이정환 - 한 입 크기로 잘라먹는 타입스크립트] 템플릿 리터럴 타입

---
+++ onebite-typescript/section8/src/chapter4.ts
@@ -0,0 +1,13 @@
+/**
+ * 템플릿 리터럴 타입
+ * 스트링 리터럴 타입을 기반으로 정해진 패턴의 문자열만 포함하는 타입
+ */
+
+type Color = "red" | "black" | "green";
+
+type Animal = "dog" | "cat" | "chicken";
+
+// type ColoredAnimal = "red-dog" | "red-cat" | "red-chicken" | "back-dog"; //...
+type ColoredAnimal = `${Color}-${Animal}`;
+
+const coloredAnimal: ColoredAnimal = "black-dog";
