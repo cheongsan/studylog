1ccef2e33f789323aa1f19ed0a2b605f4dc9bddd
Cheongsan Kim <land@cheongsan.do>
2025-03-09 23:17:35 +0900

    [#️⃣ TypeScript 이정환 - 한 입 크기로 잘라먹는 타입스크립트] 상태관리와 Props 1

---
+++ onebite-typescript/section11/src/components/Editor.tsx
@@ -0,0 +1,28 @@
+import { useState } from "react";
+
+interface Props {
+  onClickAdd: (text: string) => void;
+}
+
+export default function Editor(props: Props) {
+  // userState 함수에 초기값으로 타입을 추론해준다, 제네릭 함수
+  // 초기값이 없으면 undefined, 초기값으로 무엇이라도 전달하는게 좋다
+  const [text, setText] = useState("");
+
+  // 미리 정의된 타입을 가져와야한다. 리액트에서 이미 정의된 타입
+  const onChangeInput = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setText(e.target.value);
+  };
+
+  const onClickButton = () => {
+    props.onClickAdd(text);
+    setText("");
+  };
+
+  return (
+    <div>
+      <input value={text} onChange={onChangeInput} />
+      <button onClick={onClickButton}>추가</button>
+    </div>
+  );
+}
