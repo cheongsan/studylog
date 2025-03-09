f98d5622867191539501675f110fda52b2ac4fb3
Cheongsan Kim <land@cheongsan.do>
2025-03-09 23:48:18 +0900

    [#️⃣ TypeScript 이정환 - 한 입 크기로 잘라먹는 타입스크립트] Context API

--- onebite-typescript/section11/src/components/Editor.tsx
+++ onebite-typescript/section11/src/components/Editor.tsx
@@ -1,10 +1,13 @@
 import { useState } from "react";
+import { useTodoDispatch } from "../App";
 
 interface Props {
-  onClickAdd: (text: string) => void;
+  // onClickAdd: (text: string) => void;
 }
 
 export default function Editor(props: Props) {
+  const dispatch = useTodoDispatch();
+
   // userState 함수에 초기값으로 타입을 추론해준다, 제네릭 함수
   // 초기값이 없으면 undefined, 초기값으로 무엇이라도 전달하는게 좋다
   const [text, setText] = useState("");
@@ -15,7 +18,7 @@ export default function Editor(props: Props) {
   };
 
   const onClickButton = () => {
-    props.onClickAdd(text);
+    dispatch.onClickAdd(text);
     setText("");
   };
 
