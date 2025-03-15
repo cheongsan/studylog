9dc6099f2c4a9497f857ba65f3e5a600bc5757fb
Cheongsan Kim <land@cheongsan.do>
2025-03-15 17:34:26 +0900

    추상 클래스

--- elice_project/src/product/entities/product.entity.ts
+++ elice_project/src/product/entities/product.entity.ts
@@ -1,10 +1,11 @@
 import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+import { BaseEntity } from '../../common/base.entity';
 
 @Entity()
-export class Product {
+export class Product extends BaseEntity {
   // PrimaryGeneratedColumn가 없으면 테이블이 생성되지 않는다. [필수값]
-  @PrimaryGeneratedColumn('uuid')
-  public id: string;
+  // @PrimaryGeneratedColumn('uuid')
+  // public id: string;
 
   @Column()
   public name: string;
