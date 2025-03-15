6e334579474f0773c7aca26d6d61fa3c7bf20f93
Cheongsan Kim <land@cheongsan.do>
2025-03-15 15:53:33 +0900

    [🐱 NestJS 곽유신 - 엘리스 NestJS 원데이 부트캠프] Product 테이블 생성

--- elice_project/src/product/entities/product.entity.ts
+++ elice_project/src/product/entities/product.entity.ts
@@ -1,1 +1,20 @@
-export class Product {}
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity()
+export class Product {
+  // PrimaryGeneratedColumn가 없으면 테이블이 생성되지 않는다. [필수값]
+  @PrimaryGeneratedColumn('uuid')
+  public id: string;
+
+  @Column()
+  public name: string;
+
+  @Column()
+  public description: string;
+
+  @Column()
+  public price: number;
+
+  @Column()
+  public imageUrl: number;
+}
